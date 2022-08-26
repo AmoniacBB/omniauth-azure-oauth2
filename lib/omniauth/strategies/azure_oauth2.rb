1a294d852ddc5b58185a806bcc4dfe07576920f9
Oleksandr Simonov <oleksandr@amoniac.eu>
2022-08-26 11:32:08 +0300

    set to v2.0

--- lib/omniauth/strategies/azure_oauth2.rb
+++ lib/omniauth/strategies/azure_oauth2.rb
@@ -5,16 +5,13 @@ module OmniAuth
   module Strategies
     class AzureOauth2 < OmniAuth::Strategies::OAuth2
       BASE_AZURE_URL = 'https://login.microsoftonline.com'
-      BASE_SCOPE_URL = 'https://graph.microsoft.com/'
+      USER_INFO_URL = 'https://graph.microsoft.com/v1.0/me'
       BASE_SCOPES = %w[profile email openid].freeze
-      DEFAULT_SCOPE = 'email,profile'
+      DEFAULT_SCOPE = 'email profile'
 
       option :name, 'azure_oauth2'
 
       option :tenant_provider, nil
-
-      # AD resource identifier
-      option :resource, '00000002-0000-0000-c000-000000000000'
 
       # tenant_provider must return client_id, client_secret and optionally tenant_id and base_azure_url
       args [:tenant_provider]
@@ -37,8 +34,10 @@ module OmniAuth
         options.authorize_params = provider.authorize_params if provider.respond_to?(:authorize_params)
         options.authorize_params.domain_hint = provider.domain_hint if provider.respond_to?(:domain_hint) && provider.domain_hint
         options.authorize_params.prompt = request.params['prompt'] if defined?(request) && request.params['prompt']
-        options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/authorize"
-        options.client_options.token_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/token"
+        options.authorize_params.scope = (provider.scope if provider.respond_to?(:scope) && provider.scope) || DEFAULT_SCOPE
+        
+        options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/authorize"
+        options.client_options.token_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/token"
         super
       end
 
@@ -70,11 +69,6 @@ module OmniAuth
         }
       end
 
-      def token_params
-        azure_resource = request.env['omniauth.params'] && request.env['omniauth.params']['azure_resource']
-        super.merge(resource: azure_resource || options.resource)
-      end
-
       def callback_url
         full_host + script_name + callback_path
       end
@@ -84,14 +78,6 @@ module OmniAuth
         @raw_info ||= ::JWT.decode(access_token.token, nil, false).first
       end
 
-      private
-
-      def get_scope(params)
-        raw_scope = params[:scope] || DEFAULT_SCOPE
-        scope_list = raw_scope.split(' ').map { |item| item.split(',') }.flatten
-        scope_list.map! { |s| s =~ %r{^https?://} || BASE_SCOPES.include?(s) ? s : "#{BASE_SCOPE_URL}#{s}" }
-        scope_list.join(' ')
-      end
     end
   end
 end
